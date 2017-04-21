45b63ccd57d07580de1f33b7dac6ccb0c3ad574d
Alex_zhao <zgh0711@163.com>
2017-04-21 10:59:58 +0800

    BaseActivity提供设置状态栏颜色的方法。

--- app/src/main/java/com/zgh/appdevtemplate/base/BaseActivity.java
+++ app/src/main/java/com/zgh/appdevtemplate/base/BaseActivity.java
@@ -1,6 +1,7 @@
 package com.zgh.appdevtemplate.base;
 
 import android.os.Bundle;
+import android.support.annotation.ColorInt;
 import android.support.annotation.Nullable;
 
 import com.jaeger.library.StatusBarUtil;
@@ -18,7 +19,16 @@ public class BaseActivity extends SupportActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        StatusBarUtil.setColor(this,getResources().getColor(R.color.colorAccent));
+
+        setStatusBarColor(getResources().getColor(R.color.colorAccent));
+    }
+
+    /**
+     * 设置状态栏颜色,默认是带有 112 透明度的，
+     * 也可以重写此方法，使用 StatusBarUtil 的其他方式设置状态栏
+     */
+    protected void setStatusBarColor(@ColorInt int color) {
+        StatusBarUtil.setColor(this, color);
     }
 
     @Override
