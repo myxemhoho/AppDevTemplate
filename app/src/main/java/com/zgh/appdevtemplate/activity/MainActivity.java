bdbe0c2b5ba09e196e082703b71f30ecc29a70c4
Alex_zhao <zgh0711@163.com>
2017-05-25 17:51:22 +0800

    增加 tabLayout 库，现在可以有二种方法来实现底部导航条

--- app/src/main/java/com/zgh/appdevtemplate/activity/MainActivity.java
+++ app/src/main/java/com/zgh/appdevtemplate/activity/MainActivity.java
@@ -7,9 +7,13 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.annotation.RequiresApi;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v7.app.AlertDialog;
 import android.text.TextUtils;
 
+import com.flyco.tablayout.listener.CustomTabEntity;
 import com.zgh.appdevtemplate.R;
 import com.zgh.appdevtemplate.base.BaseActivity;
 import com.zgh.appdevtemplate.constant.Urls;
@@ -18,17 +22,72 @@ import com.zgh.appdevtemplate.fragment.MainFragment;
 import com.zgh.appdevtemplate.update.UpdataService;
 import com.zgh.appdevtemplate.util.AppUtils;
 
+import java.util.ArrayList;
+
 public class MainActivity extends BaseActivity {
+    private ArrayList<Fragment> mFragments = new ArrayList<>();
+    private ArrayList<CustomTabEntity> mTabEntities     = new ArrayList<>();
+    private String[]                   mTitles          = {"首页", "消息", "联系人", "更多"};
+    private int[]                      mIconUnselectIds = {
+            R.mipmap.tab_home_unselect, R.mipmap.tab_speech_unselect,
+            R.mipmap.tab_contact_unselect, R.mipmap.tab_more_unselect};
+    private int[]                      mIconSelectIds   = {
+            R.mipmap.tab_home_select, R.mipmap.tab_speech_select,
+            R.mipmap.tab_contact_select, R.mipmap.tab_more_select};
 
     @Override
     protected void initContentView(Bundle savedInstanceState) {
         setContentView(R.layout.activity_main);
+        //使用 ViewPager 及 Fragment 实现项目整体架构,与下面 BottomBar 的方式二选一
+//        viewPagerWithFragment();
+        //使用 BottomBar 及 Fragment 实现项目整体架构
         if (savedInstanceState == null) {
             loadRootFragment(R.id.fl_mainActivity, MainFragment.newInstance());
         }
 
         checkOption();
         checkUpdate();
+    }
+
+    private void viewPagerWithFragment() {
+//        for (int i = 0; i < mTitles.length; i++) {
+//            mTabEntities.add(new TabEntity(mTitles[i], mIconSelectIds[i], mIconUnselectIds[i]));
+//            mFragments.add(ListFragment.newInstance());
+//        }
+//        final CommonTabLayout tabLayout = (CommonTabLayout) findViewById(R.id.tabLayout);
+//        tabLayout.setTabData(mTabEntities);
+//
+//        final ViewPager viewPager = (ViewPager) findViewById(R.id.vp_main);
+//        viewPager.setAdapter(new MyPageAdapter(getSupportFragmentManager()));
+//
+//        tabLayout.setOnTabSelectListener(new OnTabSelectListener() {
+//            @Override
+//            public void onTabSelect(int position) {
+//                viewPager.setCurrentItem(position);
+//            }
+//
+//            @Override
+//            public void onTabReselect(int position) {
+//
+//            }
+//        });
+//
+//        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+//            @Override
+//            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+//
+//            }
+//
+//            @Override
+//            public void onPageSelected(int position) {
+//                tabLayout.setCurrentTab(position);
+//            }
+//
+//            @Override
+//            public void onPageScrollStateChanged(int state) {
+//
+//            }
+//        });
     }
 
     @Override
@@ -100,6 +159,27 @@ public class MainActivity extends BaseActivity {
                     })
                     .create();
             dialog.show();
+        }
+    }
+
+    private class MyPageAdapter extends FragmentPagerAdapter {
+        public MyPageAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mTitles[position];
         }
     }
 }
